970f0c52a75feb6e07d3c22b99df866a272d3f27
Teva RIOU <triou@e2r8p11.42.fr>
2018-09-17 20:35:09 +0200

    save

--- asm/includes/asm.h
+++ asm/includes/asm.h
@@ -6,7 +6,7 @@
 /*   By: triou <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/12 16:04:54 by triou             #+#    #+#             */
-/*   Updated: 2018/09/17 18:30:42 by triou            ###   ########.fr       */
+/*   Updated: 2018/09/17 20:33:55 by triou            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,32 @@
 # include "op.h"
 # include "libft.h"
 
-# define USAGE			"Usage:\t./asm [-a] <sourcefile.s>\n\t-a : Instead of " \
-						"creating a .cor file, outputs a stripped and annotated " \
-						"version of the code to the standard output\n"
-# define OPTION			"-a"
-# define EXT			".s"
+# define USAGE				"Usage:\t./asm [-a] <sourcefile.s>\n\t-a : Instead of " \
+							"creating a .cor file, outputs a stripped and annotated " \
+							"version of the code to the standard output\n"
+# define OPTION				"-a"
+# define EXT				".s"
+
+# define L_STR_LABEL		1
+# define L_LABEL_STR		2
+# define L_DIRECT			3
+# define L_SEPARATOR		4
+# define L_REG				5
+# define L_OP				6
+# define L_NUM				7
+# define L_BLANKS			8
 
 /* ERROR MESSAGES */
 # define WRONG_EXT			"Wrong filename extension\n"
 # define FILE_OVERFLOW		"File is too big\n"
 # define NAME_TOO_LONG		"Program name is too long\n"
 # define COMMENT_TOO_LONG	"Program comment is too long\n"
+
+
+
+typedef struct		s_lex {
+	t_tok			tok;
+}					t_lex;
 
 typedef struct		s_file {
 	size_t			n;
@@ -38,6 +53,7 @@ typedef struct		s_file {
 typedef struct		s_asm {
 	t_bool			option;
 	t_file			*input;
+	t_lex			*lex;
 	header_t		header;
 }					t_asm;
 
