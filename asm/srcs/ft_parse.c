8261f10c36812bbb65e9db69125cc554801e160b
Teva RIOU <triou@e2r5p10.42.fr>
2018-09-24 00:04:47 +0200

    parse4

--- asm/srcs/ft_parse.c
+++ asm/srcs/ft_parse.c
@@ -6,7 +6,7 @@
 /*   By: triou <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/23 19:40:29 by triou             #+#    #+#             */
-/*   Updated: 2018/09/23 23:37:28 by triou            ###   ########.fr       */
+/*   Updated: 2018/09/24 00:03:46 by triou            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,6 +99,16 @@ t_bool	ft_parse_3(t_file *line, t_lex **token)
 	return (TRUE);
 }
 
+t_lex	*traverse_arg(t_file *line, t_lex *list, int type)
+{
+	t_lex	*end;
+
+	end = line->tokens;
+	while (list != end && list->arg_type == type)
+			list = list->next;	
+	return (list);
+}
+
 t_bool	ft_parse_4(t_file *line, t_lex **token)
 {
 	t_lex	*list;
@@ -111,7 +121,27 @@ t_bool	ft_parse_4(t_file *line, t_lex **token)
 	if (list->token == L_BLANKS && (list = list->next) == end)
 		return (FALSE);
 	if (list->arg_type == T_DIR)
-	{
-		
-	}
+		list = traverse_arg(line, list, T_DIR);
+	else if (list->prev->token == L_BLANKS && list->arg_type == T_IND)
+		list = traverse_arg(line, list, T_IND);
+	else if (list->prev->token == L_BLANKS && list->arg_type == T_REG)
+		list = list->next;
+	else
+		return (FALSE);
+	if (list == end || list->token != L_SEPARATOR || (list = list->next) == end)
+		return (FALSE);
+	if (list->arg_type == T_DIR)
+		list = traverse_arg(line, list, T_DIR);
+	else if (list->arg_type == T_IND)
+		list = traverse_arg(line, list, T_IND);
+	else if (list->arg_type == T_REG)
+		list = list->next;
+	else
+		return (FALSE);
+	if (list == end || list->token != L_SEPARATOR || (list = list->next) == end)
+		return (FALSE);
+	if (list->arg_type != T_REG)
+		return (FALSE);
+	*token = list->next;
+	return (TRUE);
 }
