27121773a07dc79ceed9ebf83e04f4daf4c391d2
Lucien TERRAILLON <lterrail@e3r10p19.42.fr>
2018-10-03 14:41:10 +0200

    merge ?

--- vm/srcs/opp/xor.c
+++ vm/srcs/opp/xor.c
@@ -6,7 +6,7 @@
 /*   By: lmazeaud <lmazeaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/28 00:07:31 by lmazeaud          #+#    #+#             */
-/*   Updated: 2018/10/01 19:53:33 by lterrail         ###   ########.fr       */
+/*   Updated: 2018/10/02 17:07:24 by lterrail         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@
 ** Si la valeur resultante est egale a zero, alors le carry passe a l'etat un,
 ** sinon a l'etat zero.
 */
-#include <limits.h>
+
 int		xor(t_mars *mars, t_processus *process)
 {
 	printf("BONJOUR XOR\n");
---
+++ vm/test/test.s
@@ -0,0 +1,5 @@
+.name "test"
+.comment "push"
+
+add r1, r2, r3
+and r1, r2, r4
