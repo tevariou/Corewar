7689aa7abb507745f05c87e71f7fc919ef59c07f
Lmzd <mazeaud.louis@gmail.com>
2018-10-04 09:33:09 +0200

    rebase ncurses to vm

--- vm/srcs/ncurses/ft_ncurses_display.c
+++ vm/srcs/ncurses/ft_ncurses_display.c
@@ -6,7 +6,7 @@
 /*   By: lmazeaud <lmazeaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/27 17:57:11 by abiestro          #+#    #+#             */
-/*   Updated: 2018/10/02 21:25:24 by lmazeaud         ###   ########.fr       */
+/*   Updated: 2018/10/03 17:46:15 by lmazeaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,36 +15,36 @@
 #include "libft.h"
 #include "mars.h"
 
-void	ft_info_processus(t_mars *mars, t_processus *process, t_visu *visu)
+void	ft_ncurses_print_processus(t_mars *mars, t_processus *process, t_visu *visu)
 {
 	unsigned	i;
 
 	i = 0;
 	if (!process)
 		ft_exit(NULL, "no process");
-	wattron(visu->info, COLOR_PAIR(15));
+	wattron(visu->info, COLOR_PAIR(0));
 	wprintw(visu->info, "\nprocessus -> ");
-	wattroff(visu->info, COLOR_PAIR(15));
+	wattroff(visu->info, COLOR_PAIR(0));
 	wattron(visu->info, COLOR_PAIR(process->player));
 	wprintw(visu->info, "%s\n", process->name);
 	wattroff(visu->info, COLOR_PAIR(process->player));
 
-	wattron(visu->info, COLOR_PAIR(15));
+	wattron(visu->info, COLOR_PAIR(0));
 	wprintw(visu->info, "Last Cycle Live     -> ");
-	wattroff(visu->info, COLOR_PAIR(15));
+	wattroff(visu->info, COLOR_PAIR(0));
 	wattron(visu->info, COLOR_PAIR(process->player));
 	wprintw(visu->info, "%u\n", process->last_cycle_live);
 	wattroff(visu->info, COLOR_PAIR(process->player));
 
-	wattron(visu->info, COLOR_PAIR(15));
+	wattron(visu->info, COLOR_PAIR(0));
 	wprintw(visu->info, "Nbr of Live   -> ");
-	wattroff(visu->info, COLOR_PAIR(15));
+	wattroff(visu->info, COLOR_PAIR(0));
 	wattron(visu->info, COLOR_PAIR(process->player));
 	wprintw(visu->info, "%u\n", process->nbr_of_live);
 	wattroff(visu->info, COLOR_PAIR(process->player));
 }
 
-void	ft_print_process_pc(t_mars *mars, t_visu *visu)
+void	ft_ncurses_print_process_pc(t_mars *mars, t_visu *visu)
 {
 	t_processus *p;
 
@@ -59,7 +59,7 @@ void	ft_print_process_pc(t_mars *mars, t_visu *visu)
 	}
 }
 
-void	ft_info_ram(t_mars *mars, t_visu *visu)
+void	ft_ncurses_info_ram(t_mars *mars, t_visu *visu)
 {
 	int i;
 
@@ -68,27 +68,15 @@ void	ft_info_ram(t_mars *mars, t_visu *visu)
 	i = 0;
 	while (i < MEM_SIZE)
 	{
-		if (!mars->memory[i][1])
-		{
-			wattron(visu->arena, COLOR_PAIR(15));
-			mvwprintw(visu->arena, i / 64, i % 64 * 3, "%02.2hhX ", mars->memory[i][0]);
-		}
-		else if (mars->memory[i][1] == 1)
-		{
-			wattron(visu->arena, COLOR_PAIR(1));
-			mvwprintw(visu->arena, i / 64, i % 64 * 3, "%02.2hhX ", mars->memory[i][0]);
-		}
-		else if (mars->memory[i][1] == 2)
-		{
-			wattron(visu->arena, COLOR_PAIR(2));
-			mvwprintw(visu->arena, i / 64, i % 64 * 3, "%2.2hhX ", mars->memory[i][0]);
-		}
+		wattron(visu->arena, COLOR_PAIR(mars->memory[i][1]));
+		mvwprintw(visu->arena, i / 64, i % 64 * 3, "%02.2hhX ", mars->memory[i][0]);
+		wattroff(visu->arena, COLOR_PAIR(mars->memory[i][1]));
 		i++;
 	}
-	ft_print_process_pc(mars, visu);
+	ft_ncurses_print_process_pc(mars, visu);
 }
 
-void	ft_info_mars(t_mars *mars, t_visu *visu)
+void	ft_ncurses_info_mars(t_mars *mars, t_visu *visu)
 {
 	int			i;
 	t_processus *p;
@@ -101,18 +89,19 @@ void	ft_info_mars(t_mars *mars, t_visu *visu)
 	wprintw(visu->info, "cycle delta : %d --- \n\n", mars->cycle_delta);
 	wprintw(visu->info, "next cycle to die : %d --- \n\n", mars->cycle_to_die);
 	wprintw(visu->info, "cycle to die : %d --- \n\n", mars->cycle_teta);
+	wprintw(visu->info, "pause : %d --- \n\n", visu->pause);
 	p = mars->process_lst;
 	while (++i < (int)mars->count_players)
 	{
-		ft_info_processus(mars, p, visu);
+		ft_ncurses_print_processus(mars, p, visu);
 		p = p->next;
 	}
 	wrefresh(visu->info);
 }
 
-void	ft_debug_info(t_mars *mars, t_visu *visu)
+void	ft_ncurses_display(t_mars *mars, t_visu *visu)
 {
-	ft_info_mars(mars, visu);
-	ft_info_ram(mars, visu);
+	ft_ncurses_info_mars(mars, visu);
+	ft_ncurses_info_ram(mars, visu);
 	wrefresh(visu->arena);
 }
