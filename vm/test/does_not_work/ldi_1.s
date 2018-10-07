31065aa7327d4dcd796a7c06f631f32dbb003221
Lucien TERRAILLON <lterrail@e1r10p3.42.fr>
2018-10-07 18:48:31 +0200

    add /test/does_not_work

---
+++ vm/test/does_not_work/ldi_1.s
@@ -0,0 +1,5 @@
+.name "tester"
+.comment "test ldi"
+
+ldi %4, %4, r3
+st r3, 57
--- vm/test/load_store/indirect_store.s
+++ vm/test/load_store/indirect_store.s
@@ -1,7 +1,11 @@
 .name "indirectstore"
 .comment "push"
 
-sti r1, %:live, %1
-
-live:
-live %4
+ld %42, r1
+ld %5, r2
+ld %6, r3
+sti r2, %7, %1
+live %295423
+add r1, r3, r4
+add r2, r3, r5
+st r3, 50
