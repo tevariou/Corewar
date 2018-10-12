8142c6625226a16db31c63cc7a525a186515353d
Teva RIOU <triou@e1r10p5.42.fr>
2018-10-12 18:37:02 +0200

    unit test

--- resources/vm_champs/test.sh
+++ resources/vm_champs/test.sh
@@ -1,5 +1,33 @@
 #!/bin/sh
-for f in champs/*.s;
+
+cor="cor"
+dir="champs"
+
+for f in $dir/*.s
 do
-	echo $f; ./tev $f
+	./zaz $f &>/dev/null
+	if [ -f "$(echo $f | rev | cut -c2- | rev)$cor" ]
+	then
+		hexdump -C $(echo $f | rev | cut -c2- | rev)$cor > $dir/zaz
+		rm -f $(echo $f | rev | cut -c2- | rev)$cor
+	fi
+	./tev $f &>/dev/null
+	if [ -f "$(echo $f | rev | cut -c2- | rev)$cor" ]
+	then
+		hexdump -C $(echo $f | rev | cut -c2- | rev)$cor > $dir/tev
+		rm -f $(echo $f | rev | cut -c2- | rev)$cor
+	fi
+	if [ -f "$dir/tev" ] && [ -f "$dir/zaz" ]
+	then
+		echo "Testing $f"
+		diff $dir/zaz $dir/tev
+	fi
+	if [ -f "$dir/tev" ]
+	then
+		rm -f $dir/tev
+	fi
+	if [ -f "$dir/zaz" ]
+	then
+		rm -f $dir/zaz
+	fi
 done
