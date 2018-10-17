5dd576030fa2953c6790e53d6ba1f9fd995444aa
Armand BIESTRO <abiestro@e2r13p7.42.fr>
2018-10-17 16:10:40 +0200

    aff

--- vm/srcs/ending_game.c
+++ vm/srcs/ending_game.c
@@ -6,7 +6,7 @@
 /*   By: abiestro <abiestro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/25 18:29:00 by abiestro          #+#    #+#             */
-/*   Updated: 2018/10/16 18:04:54 by abiestro         ###   ########.fr       */
+/*   Updated: 2018/10/17 16:06:47 by abiestro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,18 @@ void    end_game(t_mars *mars)
 
     c = mars->champion_lst;
     winner = mars->champion_lst;
-    while (c)
-    {
-        if (c->last_cycle_live > winner->last_cycle_live)
-            winner = c;
-            c = c->next;
-    }
+    if (!mars->visualisor)
+     {
+        while (c)
+        {   
+            if (c->last_cycle_live > winner->last_cycle_live)
+                winner = c;
+                c = c->next;
+        }
     printf("\nAnd the winner is : %s \n", winner->header.prog_name);
     printf("He says: %s\n", winner->header.comment);
+    }
+    else if (mars->visualisor == NCURSE)
+		ft_close_ncurses(&mars->visu);
     ft_exit(mars, "");
 }
